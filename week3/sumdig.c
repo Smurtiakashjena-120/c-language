335cc4a0eccffb011b71780124a39fd836857c0d
SMURTI AKASH JENA <143417332+Smurtiakashjena-120@users.noreply.github.com>
2023-09-19 19:08:08 +0530

    sumdig.c

--- week3/sumdig.c
+++ week3/sumdig.c
@@ -1,24 +1,15 @@
 #include<stdio.h>
-#include<math.h>
 int main()
 {
-    int n,r,l=0,s=0;
-scanf("%d",&n);
- for (;n>0;)
+    int n,m,s=0;
+    printf("enter the number:");
+    scanf("%d",&n);
+    for (;n>0;)
     {
+        m=n%10;
         n=n/10;
-        l++;
+        s+=m;
     }
-    printf("the length is %d \n",l);
-for (;l>0;)
-{
-    r=n%10;
-    s=s+r;
-    n=n/10;
-    l=l-1;
-}
-printf("the sum is %d",s);
-
-
-    return 0;
+    printf("the sum of the digit of the number is: %d",s);
+     return 0;
 }
